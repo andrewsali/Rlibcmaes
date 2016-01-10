e6c506adce9fdcca1a4941f5d1716895d50d67e5
Andras Sali <salia@alphacruncher.local>
2016-01-10 10:47:42 +0100

    Add parallel evaluation option

--- R/cmaes.R
+++ R/cmaes.R
@@ -1,8 +1,13 @@
 #' Wrapper for block evaluation
 #' @export
-cmaes <- function(x0, sigma, optimFun, lowerB, upperB, maxevals = 1e3L) {
+cmaes <- function(x0, sigma, optimFun, lowerB, upperB, maxevals = 1e3L, cl=NULL) {
+  stopifnot(all(upperB > lowerB))
+  
   optimFunBlock <- function(x) {
-    return(as.vector(apply(x,2,optimFun)))
+    if (is.null(cl))
+      return(apply(x,2,optimFun))
+    else
+      return(parallel::parApply(cl,x,2,optimFun))
   }
   Rlibcmaes::cmaesOptim(x0, sigma, optimFun, optimFunBlock,lowerB, upperB, maxevals = 1e3L)
 }
--- tests/testthat/quadratic_test.R
+++ tests/testthat/quadratic_test.R
@@ -1,6 +1,19 @@
 # A simple test of the cmaesOptim function
 f <- function(x) {
+  Sys.sleep(.01)
   return(crossprod(x-c(.3,.5))[1])
 }
 
-test.optim <- cmaes(c(.4,.6),.1,f,c(0.35,0),c(1,1))
+single.time <- system.time(test.optim <- cmaes(c(.4,.6),.1,f,c(0.35,0),c(1,1)))
+print(single.time)
+
+# now do parallel run
+
+cl <- parallel::makeCluster(4)
+
+parallel.time <- system.time(test.optim.parallel <- cmaes(c(.4,.6),.1,f,c(0.35,0),c(1,1),cl=cl))
+
+print(parallel.time)
+
+parallel::stopCluster(cl)
+
