83eed33b72a42ee819084358eeaae486a4e85689
andras <sali.andrew@gmail.com>
2016-01-13 21:09:24 +0100

    Fix include of RcppEigen

--- src/eo_matrix.h
+++ src/eo_matrix.h
@@ -23,13 +23,11 @@
 #define EO_MATRIX_H
 
 #include <algorithm>
-#include <Eigen/Dense>
+#include "RcppEigen.h"
 #include <stdlib.h>
 
 typedef Eigen::MatrixXd dMat; // declares a column-major non-sparse matrix type of double
 typedef Eigen::VectorXd dVec; // declares a vector of double.
-
-#include <unsupported/Eigen/MatrixFunctions>
 
 inline void removeRow(dMat& matrix, unsigned int rowToRemove)
 {
