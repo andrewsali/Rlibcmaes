37f3d4cf1582baed933ca2512a1ab241f526183d
Andras Sali <sali.andrew@gmail.com>
2017-08-15 14:22:22 +0200

    Update augmented lagrangian for multiple constraints

--- R/cmaes.R
+++ R/cmaes.R
@@ -1,7 +1,7 @@
 #' Wrapper for the libcmaes library to run state-of-the art local / global optimization using CMA-ES variants. See \href{https://github.com/beniz/libcmaes/}{libcmaes} and \href{https://www.lri.fr/~hansen/cmaesintro.html}{CMA-ES webpage} for further details.
 #' @param x0 Initial guess  for parameters (mean)
 #' @param optimFun R function to be minimized
-#' @param ineqFun An R function that specifies the inequality constraint
+#' @param ineqFun A list of R functions that specifies the inequality constraint
 #' @param lower Lower bounds for parameters
 #' @param upper Upper bounds for parameters
 #' @param params The CMA-ES parameters, can be set with \code{\link{cmaEsParams}}
@@ -18,30 +18,42 @@ cmaes <- function(x0, optimFun, ineqFun, lower, upper, params=cmaEsParams(), cl=
   } 
   
   # the previous_population
-  penalty_level <- 1
-  n_iter <- 1
+  penalty_level <- rep(1,length(ineqFun))
+
+  if (is.null(cl)) {
+    apply_rows <- function(x,f) apply(x,2,f)
+  } else {
+    apply_rows <- function(x,f) parallel::parApply(cl,x,2,f)
+  }
+  
+  n_iter <- 0
   
   optimFunBlock <- function(x) {
-    if (n_iter %% 10==0) {
-      print(sprintf("Penalty level:%s",penalty_level))
+    n_iter <<- n_iter + 1
+    
+    fun_vec <- apply_rows(x,optimFun) 
+    
+    penalty_mat <- matrix(0,length(ineqFun),nrow(x))
+    
+    for (i in 1:length(ineqFun)) {
+      penalty_mat[i,] <- apply_rows(x,ineqFun) * penalty_level[i]
     }
     
-    n_iter <<- n_iter+1
-    if (is.null(cl)) {
-      penalty_vec <- apply(x,2,ineqFun) * penalty_level
-      fun_vec <- apply(x,2,optimFun) 
-    }  else {
-      penalty_vec <- parallel::parApply(cl,x,2,ineqFun) * penalty_level 
-      fun_vec <- parallel::parApply(cl,x,2,optimFun)
+    if (n_iter %% 10==0) {
+      print(sprintf("Penalty level:%s",paste(penalty_level,collapse=",")))
     }
-
-    if (penalty_vec[which.min(penalty_vec + fun_vec)] > 0) {
-      penalty_level <<- penalty_level * 1.01
-    } else {
-      penalty_level <<- penalty_level / 1.01
+    
+    optimal_element <- which.min(colSums(outer(rep(1,length(ineqFun)),fun_vec)+penalty_mat))
+    
+    for (i in 1:length(ineqFun)) {
+      if (penalty_mat[i,optimal_element]>0) {
+        penalty_level[i] <<- penalty_level[i] * 1.01
+      } else {
+        penalty_level[i] <<- penalty_level[i] / 1.01
+      }
     }
-
-    fun_vec + penalty_vec
+  
+    fun_vec + colSums(penalty_mat)
   }
   Rlibcmaes::cmaesOptim(x0, params$sigma, optimFun, optimFunBlock,lower, upper, cmaAlgo = as.integer(params$cmaAlgorithm), lambda = ifelse(is.null(params$lambda),-1,params$lambda), maxEvals = params$maxEvals, xtol=params$xtol, ftol=params$ftol, traceFreq =params$trace, seed = params$seed, quietRun=params$quiet)
 }
